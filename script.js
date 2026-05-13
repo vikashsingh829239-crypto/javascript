36e5ae33216c136684ba62507d323bacc6efe885
vikash singh <vikashsingh829239@gmail.com>
2026-05-13 12:28:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+console.log("vikash");
+var a=5;
+let b=4;
+let c="vikash"
+console.log(a+b);
+console.log(typeof a,typeof b,typeof c);
+{
+    // var a=66;
+    var a=0;
+    let b=7;
+    // console.log(b);
+}
+console.log(b);
+    
+// console.log(a);
+// const a2=8;
+// a2=a2+1; not allowed
+
+let a1="vikash";
+let b1=70;
+const c1=true;
+let d=null;
+let e=undefined;
+console.log(a1,b1,c1,d,e) 
+console.log(typeof a1,typeof b1,typeof c1,typeof d,typeof e);
+
+//object
+let students={
+    name:"vikash",
+    age:22,
+    passion:"students",
+    
+}
+console.log(students);
+students.room=67;
+students.sub= "js";
+console.log(students.room);
+
